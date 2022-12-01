4dea02b5b5abb95ba6945307f63016ada3ec440e
Matteo Kinkela <mkinkela123@gmail.com>
2022-12-02 00:46:23 +0100

    Add google site verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Matteo Kinkela - software engineer</title>
         <meta name="description" content="Matteo Kinkela - software engineer" />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="google-site-verification"
+          content="jI9gtzysCR3p9InY00qz69ZAhR5YrkkjsIn46HBz3OU"
+        />
       </Head>
       <Component {...pageProps} />
     </>
