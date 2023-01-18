6fe07dc6e936dfa25b725b0752f3d6a5587609dd
mkinkela1 <mkinkela123@gmail.com>
2023-01-18 21:41:30 +0000

    Deploying to gh-pages from @ mkinkela1/mkinkela1.github.io@d68dbbddb41ab6f81cfb966290c883fa25ce8779 🚀

--- _next/static/4aaVTGS3i-SRjgCdzNdDO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-8159065a1ee28347.js","static/css/7933760dc3471058.css","static/chunks/pages/index-705549479ecd45ee.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
